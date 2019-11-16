228bb6172023711ec0f9c6a49f72759f1406c06f
Gareth Brown <gaz@gbmail.me>
2019-11-16 18:21:39 +0000

    modify project_roles variable name

--- gcp/project.tf
+++ gcp/project.tf
@@ -1,1 +1,28 @@
+variable "project_id" {}
+variable "role_bindings" {}
+variable "project_name" {}
+variable "org_id" {}
+variable "folder_id" {}
+variable "region" {}
+variable "skip_delete" {}
+
+provider "google" {
+ region = "${var.region}"
+}
+
+resource "google_project" "project" {
+ name            = "${var.project_name}"
+ project_id      = "${var.project_id}"
+ billing_account = "${var.billing_id}"
+}
+
+resource "google_project_service" "project" {
+  project = "${google_project.project.project_id}"
+  service = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+output "project_id" {
+ value = "${google_project.project.project_id}"
+}
 
