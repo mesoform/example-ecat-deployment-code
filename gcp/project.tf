91b11596cd05fc7627f55871f24ea7cf4de94428
Daniel Perez <58601098+danperezsan@users.noreply.github.com>
2020-01-15 17:16:28 +0100

    Replace folder_id by org_id on resource google_project

--- gcp/project.tf
+++ gcp/project.tf
@@ -10,7 +10,7 @@ resource "google_project" "project" {
  name            = var.project_name
  project_id      = var.project_id
  billing_account = var.billing_id
- folder_id       = var.folder_id
+ org_id          = var.org_id
  skip_delete     = var.skip_delete
 }
 
